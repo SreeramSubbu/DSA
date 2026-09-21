ca2b7ee3f03f3a1796d3f9fd7d18f354da8f72e5
Sreeram <sreeramsiyer@gmail.com>
2026-09-21 22:30:32 +0100

    failing test

---
+++ Two Pointer/three_sum.py
@@ -0,0 +1,40 @@
+
+import unittest
+
+
+class Solution:
+    def three_sum(nums: list):
+        nums.sort
+        result = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return result
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_array_1(self):
+        vals = [-1, 0, 1, 2, -1, -1]
+        result = Solution.three_sum(vals)
+        self.assertEqual(result, [[-1, 2, -1], [0, 1, -1]])
+
+
+if __name__ == '__main__':
+    unittest.main()
